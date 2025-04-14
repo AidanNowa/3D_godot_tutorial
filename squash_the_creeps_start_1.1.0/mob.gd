f5b013192e226caae3665b9932fb84dfe61032ce
AidanNowa <aidannow@bu.edu>
2025-04-14 00:51:21 -0400

    mob script and scene

---
+++ squash_the_creeps_start_1.1.0/mob.gd
@@ -0,0 +1,23 @@
+extends CharacterBody3D
+
+@export var min_speed = 10 #m/s
+@export var max_speed = 18 #m/s
+
+func _physics_process(delta):
+	move_and_slide()
+	
+#function called from the main scene
+func initialize(start_position, player_position):
+	#position mob at start_position and rotate it towards player
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	#rotate mob randomly within range of +/-45 deg to add variation
+	rotate_y(randf_range(-PI/4, PI/4))
+	
+	#calc a random int
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	#rotate the velocity vector based on the mob's Y rotation to move in direction mob is looking
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free() #del mob when off screen
